5861e0b136e97c999dbc8799f0d22fc06141dfc0
Ke Liu <ke.liu1@thoughtworks.com>
2020-09-12 23:48:24 +0800

    [lk] add problem1

--- src/main/java/com/thoughtworks/capability/gtb/controller/EventController.java
+++ src/main/java/com/thoughtworks/capability/gtb/controller/EventController.java
@@ -1,21 +1,18 @@
 package com.thoughtworks.capability.gtb.controller;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.thoughtworks.capability.gtb.vo.EventType;
 import com.thoughtworks.capability.gtb.vo.EventVo;
 import com.thoughtworks.capability.gtb.vo.UserVo;
-import java.time.LocalDateTime;
-import java.util.Date;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 
 @RestController
 @Slf4j
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class EventController {
-
   @GetMapping("/events/{id}")
   public EventVo getEventById(@PathVariable("id") String id) {
     UserVo user = new UserVo("3", "张三");
--- src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
@@ -1,18 +1,23 @@
 package com.thoughtworks.capability.gtb.vo;
 
-import java.util.Date;
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.thoughtworks.capability.gtb.serializer.DataSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Date;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class EventVo {
-
   private String id;
   private String name;
   private EventType type;
+  @JsonSerialize(using=DataSerializer.class)
   private Date time;
+  @JsonUnwrapped
   private UserVo user;
 }
